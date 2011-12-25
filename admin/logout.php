123cb9446e0e99bd16149a20923eb51a851b19a1
Venkata Giri Reddy <venkatagirireddy@gmail.com>
2011-12-25 17:51:47 +0530

    Fixed error in path for require_once method

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,6 @@
 <?php
    include_once('../_/init.php');
-   if(!$session->isLoggedIn()) { redirect_to('login.php'); }
+   if(!$session->is_logged_in()) { redirect_to('login.php'); }
    $session->logout();
    redirect_to('login.php');
 ?>
