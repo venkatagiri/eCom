8f95c49e18c99240ca8f477840c1bdb20f5c914b
Venkata Giri Reddy <venkatagirireddy@gmail.com>
2012-03-04 19:10:36 +0530

    Modified navigation to make use of the key column of category.

--- core/templates/navigation.php
+++ core/templates/navigation.php
@@ -4,14 +4,14 @@
 <nav>
   <ul class="wrapper">
   <?php foreach($main_categories as $main_category): 
-        $main_path = "/".get_key($main_category->name)."/".$main_category->id;
+        $main_path = "/".$main_category->key."/".$main_category->id;
   ?>
     <li class="category">
       <a href="<?php echo $main_path; ?>"><?php echo $main_category->name; ?></a>
       <table class="sub_nav"><tr><td class="col">
         <h6 class="header">Categories</h6>
         <?php foreach($main_category->visible_children() as $sub_category): 
-            $path = "/".get_key($main_category->name)."/".get_key($sub_category->name)."/".$sub_category->id;
+            $path = "/".$main_category->key."/".$sub_category->key."/".$sub_category->id;
         ?>
         <a href="<?php echo $path; ?>"><?php echo $sub_category->name; ?></a>
         <?php endforeach; ?>
