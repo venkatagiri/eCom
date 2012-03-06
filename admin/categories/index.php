24e7e85b5316338e7c3eb68043355151e0edb6a6
Venkata Giri Reddy <venkatagirireddy@gmail.com>
2012-03-06 23:31:21 +0530

    Changed the class from add_category to new_entry to allow use of the tree-structure, in other places.

--- admin/categories/index.php
+++ admin/categories/index.php
@@ -26,13 +26,13 @@ window.onload = function() {
   <li class="node opened">
     <span class="toggle"></span>
     <span class="header"><?php echo $root->name; ?></span>
-    <a href="new?parent_id=<?php echo $root->id ?>" title="Add a sub Category" class="add_category">+</a>
+    <a href="new?parent_id=<?php echo $root->id ?>" title="Add a sub Category" class="new_entry">+</a>
     <ul class="list">
     <?php foreach($main_categories as $main_category) { ?>
       <li class="node closed">
         <span class="toggle"></span>
         <a href="show?id=<?php echo $main_category->id; ?>" class="header"><?php echo $main_category->name; ?></a>
-        <a href="new?parent_id=<?php echo $main_category->id; ?>" title="Add a sub Category" class="add_category">+</a>
+        <a href="new?parent_id=<?php echo $main_category->id; ?>" title="Add a sub Category" class="new_entry">+</a>
         <ul class="list">
         <?php foreach($main_category->children() as $sub_category) { ?>
           <li class="node leaf">
