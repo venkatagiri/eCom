9f694818e09a331d06cfe56a8fb797a5d7c39d6d
Venkata Giri Reddy <venkatagirireddy@gmail.com>
2012-03-11 16:23:25 +0530

    Moved the orders and customers links to Sales.

--- core/templates/admin.navigation.php
+++ core/templates/admin.navigation.php
@@ -12,17 +12,17 @@
         <a href="/admin/attributes">Attributes</a>
       </td></tr></table>
     </li>
-    <li>
-      <a href="/admin/orders">Orders</a>
-    </li>
-    <li>
-      <a href="/admin/customers">Customers</a>
+    <li class="category">
+      <a>Sales</a>
+      <table class="sub_nav"><tr><td class="col">
+        <a href="/admin/orders">Orders</a>
+        <a href="/admin/customers">Customers</a>
+      </td></tr></table>
     </li>
     <li class="category">
       <a>Extensions</a>
       <table class="sub_nav"><tr><td class="col">
         <a href="/admin/banners">Banners</a>
-        <a href="/admin/featured">Featured</a>
       </td></tr></table>
     </li>
     <?php if(is_logged_in()) { ?>
