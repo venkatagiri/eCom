a95c4c3d16c7a9baa39eba191394819ae188b91c
Venkata Giri Reddy <venkatagirireddy@gmail.com>
2012-03-11 16:28:18 +0530

    Added the 404 page for admin panel.

---
+++ admin/404.php
@@ -0,0 +1,6 @@
+<?php get_admin_header('Page Not Found'); ?>
+
+<h1>Oops!</h1>
+<p>Something has gone wrong, dont worry our tech ninjas have been informed and will resolve the issue as soon as possible.</p>
+
+<?php get_admin_footer(); ?>
