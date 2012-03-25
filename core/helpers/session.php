1a76e155f5be32c18528171a409691747b51e319
Venkata Giri Reddy <venkatagirireddy@gmail.com>
2012-03-25 20:24:19 +0530

    Added set and get methods to set/get session variables.

--- core/helpers/session.php
+++ core/helpers/session.php
@@ -1,49 +1,58 @@
 <?php
+
 class Session {
-   private $logged_in = false;
-   public $user_id;
-   public $msg;
-   function __construct() {
-      session_start();
-      $this->check_msg();
-      $this->check_login();
-   }
-   public function check_login() {
-      if(isset($_SESSION['user_id'])) {
-         $this->user_id = $_SESSION['user_id'];
-         $this->logged_in = true;
-      } else {
-         unset($this->user_id);
-         $this->logged_in = false;
-      }
-   }
-   public function login($user) {
-      if($user) {
-         $this->user_id = $_SESSION['user_id'] = $user;
-         $this->logged_in = true;
-      }
-   }
-   public function logout() {
-      unset($_SESSION['user_id']);
+  private $logged_in = false;
+  public $user_id;
+  public $msg;
+  function __construct() {
+    session_start();
+    $this->check_msg();
+    $this->check_login();
+  }
+  public function check_login() {
+    if(isset($_SESSION['user_id'])) {
+      $this->user_id = $_SESSION['user_id'];
+      $this->logged_in = true;
+    } else {
       unset($this->user_id);
       $this->logged_in = false;
-   }
-   public function is_logged_in() {
-      return $this->logged_in;
-   }
-   private function check_msg() {
-      if(isset($_SESSION['msg'])) {
-         $this->msg = $_SESSION['msg'];
-         unset($_SESSION['msg']);
-      }
-   }
-   public function message($msg='') {
-      if(empty($msg)) {
-         return $this->msg;
-      } else {
-         $_SESSION['msg'] = $msg;
-      }
-   }
+    }
+  }
+  public function login($user) {
+    if($user) {
+      $this->user_id = $_SESSION['user_id'] = $user;
+      $this->logged_in = true;
+    }
+  }
+  public function logout() {
+    unset($_SESSION['user_id']);
+    unset($this->user_id);
+    $this->logged_in = false;
+  }
+  public function is_logged_in() {
+    return $this->logged_in;
+  }
+  private function check_msg() {
+    if(isset($_SESSION['msg'])) {
+      $this->msg = $_SESSION['msg'];
+      unset($_SESSION['msg']);
+    }
+  }
+  public function message($msg='') {
+    if(empty($msg)) {
+      return $this->msg;
+    } else {
+      $_SESSION['msg'] = $msg;
+    }
+  }
+
+  public function set($key, $value) {
+    $_SESSION[$key] = $value;
+  }
+  public function get($key) {
+    if(isset($_SESSION[$key])) return $_SESSION[$key];
+    return false;
+  }
 }
 
 $session = new Session();
